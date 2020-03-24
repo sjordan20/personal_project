7a6701b1219d640466be57b2bad0a579dc769228
sjordan20 <jordanstephen949@gmail.com>
2020-03-24 00:31:12 -0600

    styling

--- src/components/editPost/EditPost.js
+++ src/components/editPost/EditPost.js
@@ -1,48 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import './editPost.css'
 
 class EditPost extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             content: props.content
-        }
-
+        };
     }
 
     handleCancel = () => {
-        this.setState({ content: this.props.content })
-        this.props.toggleEdit()
-    }
+        this.setState({ content: this.props.content });
+        this.props.toggleEdit();
+    };
 
     handleEditChange = (event) => {
-        this.setState({ content: event.target.value })
-    }
+        this.setState({ content: event.target.value });
+    };
     render() {
         // console.log(this.props.content)
         return (
-            <div>
-                <textarea
-                    rows='15'
-                    cols='40'
-                    value={this.state.content}
-                    onChange={this.handleEditChange}
-                />
-                <button
-                    onClick={() => this.handleCancel()}
-                >
-                    Cancel Edit
-                </button>
-                <button
-                    onClick={() => {
-                        this.props.editPost(this.props.id, this.state.content)
-                        this.props.toggleEdit()
-                    }}
+            <div className="body">
+                <div className="post">
+                    <textarea
+                        rows="15"
+                        cols="40"
+                        value={this.state.content}
+                        onChange={this.handleEditChange}
+                    />
 
-                >
-                    Save Edit
-                </button>
-            </div >
+                    <div className="cancel-save-container">
+                        <div className="cancel-button-container">
+                            <button className='cancel-button'
+                                onClick={() => this.handleCancel()}>Cancel Edit</button>
+                        </div>
+
+                        <div className='save-button-container'>
+                            <button
+                                className='save-button'
+                                onClick={() => {
+                                    this.props.editPost(this.props.id, this.state.content);
+                                    this.props.toggleEdit();
+                                }}
+                            >
+                                Save Edit
+						</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="bumper"></div>
+            </div>
         );
     }
 }
--- src/components/userPost/UserPost.js
+++ src/components/userPost/UserPost.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import './userPost.css'
 
 function UserPost(props) {
     return (
         <div className='body'>
+            <div className='post'>
+                <img className="post-photo" src={props.photo} />
+                <div className="post-content">
 
-            <img className="post-photo" src={props.photo} />
-            <div className="post-content">
+                    {props.content}
+                </div>
 
-                {props.content}
+
+                <div className='edit-delete-container'>
+                    <div className='edit-button-container'>
+                        <button className='edit-button'
+                            onClick={props.toggleEdit}
+                        >Edit</button>
+                    </div>
+                    <div className='delete-button-container'>
+                        <button className='delete-button'
+                            onClick={() => {
+                                props.deletePost(props.id)
+                            }}
+                        >
+                            Delete
+            </button>
+                    </div>
+                </div>
+
+
             </div>
 
-            <button
-                onClick={props.toggleEdit}
-            >edit</button>
-            <button
-                onClick={() => {
-                    props.deletePost(props.id)
-                }}
-            >
-                delete
-            </button>
+
 
             <div className='bumper'></div>
 
